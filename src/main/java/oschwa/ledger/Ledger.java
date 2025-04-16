92f2ef874ba7cda841494713deeac593bc01b53b
oschwa <oschwab33@gmail.com>
2025-04-15 17:36:22 -0700

    initial commit

---
+++ src/main/java/oschwa/ledger/Ledger.java
@@ -0,0 +1,17 @@
+package oschwa.ledger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ledger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
