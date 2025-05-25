6c7da3076d99fe7004ac61a1a1be44bbd838b895
oschwa <oschwab33@gmail.com>
2025-05-24 21:23:05 -0700

    Add basic add command feature

---
+++ src/main/java/oschwa/ledger/commands/AddCommand.java
@@ -0,0 +1,34 @@
+package oschwa.ledger.commands;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import oschwa.ledger.registries.LedgerGroupRegistry;
+
+public class AddCommand implements CommandExecutor {
+
+    private Server server;
+    private LedgerGroupRegistry ledgerGroupRegistry;
+
+    public AddCommand(Server server, LedgerGroupRegistry ledgerGroupRegistry) {
+        this.server = server;
+        this.ledgerGroupRegistry = ledgerGroupRegistry;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
+                             @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (!(commandSender instanceof Player)) return false;
+
+        Player newPlayer = server.getPlayer(strings[0]);
+
+        if (newPlayer == null) return false;
+
+        ledgerGroupRegistry.getGroup((Player)commandSender).addMember(newPlayer);
+
+        return true;
+    }
+}
