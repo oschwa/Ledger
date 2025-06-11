762372676afdece51ac1ef4f798e9c923e38705b
oschwa <oschwab33@gmail.com>
2025-06-11 10:10:17 -0700

    Add exception handling for new command

--- src/main/java/oschwa/ledger/commands/NewCommand.java
+++ src/main/java/oschwa/ledger/commands/NewCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import oschwa.ledger.exceptions.GroupExistsException;
 import oschwa.ledger.registries.LedgerGroupRegistry;
 
 public class NewCommand implements CommandExecutor {
@@ -18,7 +19,11 @@ public class NewCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
         if (!(commandSender instanceof Player)) return false;
 
-        ledgerGroupRegistry.addGroup((Player)commandSender);
+        try {
+            ledgerGroupRegistry.addGroup((Player)commandSender);
+        } catch (GroupExistsException e) {
+            commandSender.sendMessage(e.getMessage());
+        }
 
         return true;
     }
