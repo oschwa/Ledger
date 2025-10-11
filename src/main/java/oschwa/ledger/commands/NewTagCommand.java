b7b76a1b6b8d6285268e25509c8dae0deaafaaa5
oschwa <oschwab33@gmail.com>
2025-10-11 15:43:39 -0700

    add basic new tag command

---
+++ src/main/java/oschwa/ledger/commands/NewTagCommand.java
@@ -0,0 +1,21 @@
+package oschwa.ledger.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import oschwa.ledger.enums.LedgerErrorMessage;
+
+public class NewTagCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
+                             @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (!(commandSender instanceof Player)) {
+            LedgerErrorMessage.PLAYER_ONLY.send(commandSender);
+            return true;
+        }
+
+        return true;
+    }
+}
