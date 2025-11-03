fa28c0629e9b91ff47fd49554f084d83fa603f29
oschwa <oschwab33@gmail.com>
2025-11-02 21:44:54 -0800

    removed deprecated exception classes

--- src/main/java/oschwa/ledger/listeners/LabelListener.java
+++ src/main/java/oschwa/ledger/listeners/LabelListener.java
@@ -1,4 +1,23 @@
 package oschwa.ledger.listeners;
+import org.bukkit.Material;
+import org.bukkit.block.Chest;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
-public class LabelListener implements Listener {}
+public class LabelListener implements Listener {
+    @EventHandler
+    public void onLabelToChestInteract(PlayerInteractEvent event) {
+
+        if (!(event.getClickedBlock() instanceof Chest)) return;
+
+        ItemStack item = event.getPlayer().getActiveItem();
+
+        if (item.getType() != Material.NAME_TAG) return;
+
+
+
+
+    }
+}
