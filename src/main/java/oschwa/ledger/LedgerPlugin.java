9eb88a6cae16078a430d9475ddf22c3da6044b12
oschwa <oschwab33@gmail.com>
2025-11-11 13:05:41 -0800

    refactor command and listener registration

--- src/main/java/oschwa/ledger/LedgerPlugin.java
+++ src/main/java/oschwa/ledger/LedgerPlugin.java
@@ -3,17 +3,35 @@ package oschwa.ledger;
 import org.bukkit.plugin.java.JavaPlugin;
 import oschwa.ledger.commands.*;
 import oschwa.ledger.listeners.LabelListener;
+import oschwa.ledger.registries.ChestRegistry;
 import oschwa.ledger.registries.LedgerRegistry;
 
 public class LedgerPlugin extends JavaPlugin {
 
     private static LedgerPlugin plugin;
     private final LedgerRegistry ledgerRegistry = new LedgerRegistry();
+    private final ChestRegistry chestRegistry = new ChestRegistry();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         plugin = this;
+        registerCommands();
+        registerListeners();
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public static LedgerPlugin getPlugin() {
+        return plugin;
+    }
+
+    public void registerListeners() {
+        getServer().getPluginManager().registerEvents(new LabelListener(chestRegistry), this);
+    }
+
+    public void registerCommands() {
         this.getCommand("man").setExecutor(new ManualCommand());
         this.getCommand("new").setExecutor(new NewCommand(ledgerRegistry));
         this.getCommand("scrap").setExecutor(new ScrapCommand(ledgerRegistry));
@@ -21,16 +39,5 @@ public class LedgerPlugin extends JavaPlugin {
         this.getCommand("list").setExecutor(new ListCommand(ledgerRegistry));
         this.getCommand("leave").setExecutor(new LeaveCommand(ledgerRegistry));
         this.getCommand("new_label").setExecutor(new NewLabelCommand(ledgerRegistry));
-
-        getServer().getPluginManager().registerEvents(new LabelListener(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    public static LedgerPlugin getPlugin() {
-        return plugin;
     }
 }
