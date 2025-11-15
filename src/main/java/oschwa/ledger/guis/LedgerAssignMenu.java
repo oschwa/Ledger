75db2891fa76b1518885647e32eaab9b240c4160
oschwa <oschwab33@gmail.com>
2025-11-15 12:09:00 -0800

    add feature for player inventory repopulation upon label menu closure

--- src/main/java/oschwa/ledger/guis/LedgerAssignMenu.java
+++ src/main/java/oschwa/ledger/guis/LedgerAssignMenu.java
@@ -17,21 +17,40 @@ import java.util.Map;
 public class LedgerAssignMenu implements Listener {
 
     private final Player player;
+
     private Inventory menu;
 
-    private Map<ItemStack, Integer> prevIndexMap;
+    private Map<Integer, ItemStack> prevIndexMap;
 
     private Label label;
-    private final int size;
+
+    private final int SIZE = 36;
 
     public LedgerAssignMenu(Player player, Label label) {
         this.player = player;
+
         this.label = label;
-        size = 36;
-        menu = Bukkit.createInventory(player, size,
+
+        menu = Bukkit.createInventory(player, SIZE,
                 "Assign items to " + label.getName());
 
         this.prevIndexMap = new HashMap<>();
+
+        //  clone player inventory to repopulate
+        //  upon menu close.
+
+        Inventory inventory = player.getInventory();
+
+        for (int i = 0; i < inventory.getSize(); i++) {
+
+            ItemStack item = inventory.getItem(i);
+
+            if (item != null) {
+
+                prevIndexMap.put(i, item.clone());
+
+            }
+        }
 
         Bukkit.getServer().getPluginManager()
                 .registerEvents(this, LedgerPlugin.getPlugin());
@@ -53,27 +72,20 @@ public class LedgerAssignMenu implements Listener {
     }
 
     @EventHandler
-    public void onItemSelect(InventoryClickEvent e) {
-        if (e.getClickedInventory() != menu) return;
-
-        ItemStack cursor = e.getCursor();
-
-        if (cursor.getType().isAir()) return;
-
-        if (prevIndexMap.containsKey(cursor) &&
-                prevIndexMap.get(cursor) == e.getSlot()) {
-            prevIndexMap.remove(cursor);
-        }
-
-        prevIndexMap.put(cursor, e.getSlot());
-    }
-
-    @EventHandler
     public void onMenuClose(InventoryCloseEvent e) {
+        if (!(e.getPlayer() instanceof Player)) return;
+
+        if (!(e.getPlayer().equals(this.player))) return;
+
+        if (!(e.getInventory().equals(menu))) return;
+
         Inventory inventory = player.getInventory();
 
-        for (Map.Entry<ItemStack, Integer> entry : prevIndexMap.entrySet()) {
-            inventory.setItem(entry.getValue(), entry.getKey());
+        for (Map.Entry<Integer, ItemStack> entry : prevIndexMap.entrySet()) {
+
+            inventory.setItem(entry.getKey(), entry.getValue());
+
         }
+
     }
 }
